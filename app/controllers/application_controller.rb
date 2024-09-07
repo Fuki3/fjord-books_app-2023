fc43cd6e0a998927903540ad42193fe26f46d894
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2024-09-07 21:52:18 +0900

    i18nを使ってアプリを国際化しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
 end
