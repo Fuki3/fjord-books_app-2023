488f8760ca892834e439b1895fe6d5576af778e4
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-05-19 23:02:26 +0900

    has_manyの定義をbelongs_toの下に移動

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'mentioning_id', inverse_of: :mentioning, dependent: :destroy
+  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'mentioned_id', inverse_of: :mentioned, dependent: :destroy
+  has_many :mentioning_reports, through: :active_relationships, source: :mentioned
+  has_many :mentioned_reports, through: :passive_relationships, source: :mentioning
 
   validates :title, presence: true
   validates :content, presence: true
@@ -22,9 +26,4 @@ class Report < ApplicationRecord
   def unmention(other_report_id)
     active_relationships.find_by(mentioned_id: other_report_id).destroy
   end
-
-  has_many :active_relationships, class_name: 'Relationship', foreign_key: 'mentioning_id', inverse_of: :mentioning, dependent: :destroy
-  has_many :passive_relationships, class_name: 'Relationship', foreign_key: 'mentioned_id', inverse_of: :mentioned, dependent: :destroy
-  has_many :mentioning_reports, through: :active_relationships, source: :mentioned
-  has_many :mentioned_reports, through: :passive_relationships, source: :mentioning
 end
