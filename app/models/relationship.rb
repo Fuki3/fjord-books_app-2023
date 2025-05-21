e47086653d80bf8d79084ef4f6c56039bbe58c96
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-05-21 22:32:11 +0900

    モデル側にバリデーションを設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,8 @@
 class Relationship < ApplicationRecord
   belongs_to :mentioning, class_name: 'Report'
   belongs_to :mentioned, class_name: 'Report'
+
+  validates :mentioning_id, presence: true
+  validates :mentioned_id, presence: true
+  validates :mentioning_id, uniqueness: { scope: :mentioned_id }
 end
