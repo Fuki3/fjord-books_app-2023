374515729f7073d5c548a35ecad76476bfafedab
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-06-01 23:13:59 +0900

    不要なバリデーションの削除

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,7 +4,5 @@ class Relationship < ApplicationRecord
   belongs_to :mentioning, class_name: 'Report'
   belongs_to :mentioned, class_name: 'Report'
 
-  validates :mentioning_id, presence: true
-  validates :mentioned_id, presence: true
   validates :mentioning_id, uniqueness: { scope: :mentioned_id }
 end
