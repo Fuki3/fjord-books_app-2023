a47b8bca41f66e86e985788aee86b0675b43c7af
ktmmi <youemail@example.com(sooooktmmi@gmail.com)>
2025-08-09 00:07:58 +0900

    モデルテストとシステムテストの追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable?' do
+    user = users(:alice)
+    report = reports(:one)
+    assert report.editable?(user)
+  end
+
+  test '#created_on' do
+    report = reports(:one)
+    assert_equal report.created_at.to_date, report.created_on
+  end
 end
